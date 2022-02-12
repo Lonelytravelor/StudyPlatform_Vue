704c03f0fbe1795ff65a00366696111b8f2cda98
Pandora <1746384966@qq.com>
2022-02-13 00:45:06 +0800

    完善了搜索栏，新增，批量删除，导入导出，每行中的编辑和删除，面包屑和分页控件等功能

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home" style="height: 100%">
     <el-container style="height: 100%; ">
-      <el-aside :width="sideWidth  + 'px'" style="background-color: rgb(238, 241, 246);height: 100%">
+      <el-aside :width="sideWidth  + 'px'" style="background-color: rgb(238, 241, 246); box-shadow: 2px 0 6px rgb(0,21,41)">
         <el-menu :default-openeds="['1', '3']" style="height: 100%; overflow-x: hidden"
                  background-color="rgb(48,65,86)" text-color="#ffffff" active-text-color="#ffd04b"
                   :collapse-transition="false"
@@ -15,12 +15,7 @@
               <span slot="title">导航一</span>
             </template>
             <el-menu-item-group>
-              <template slot="title">分组一</template>
               <el-menu-item index="1-1">选项1</el-menu-item>
-              <el-menu-item index="1-2">选项2</el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="分组2">
-              <el-menu-item index="1-3">选项3</el-menu-item>
             </el-menu-item-group>
             <el-submenu index="1-4">
               <template slot="title">选项4</template>
@@ -32,29 +27,16 @@
               <span slot="title">导航二</span>
             </template>
             <el-menu-item-group>
-              <template slot="title">分组一</template>
               <el-menu-item index="2-1">选项1</el-menu-item>
-              <el-menu-item index="2-2">选项2</el-menu-item>
             </el-menu-item-group>
-            <el-menu-item-group title="分组2">
-              <el-menu-item index="2-3">选项3</el-menu-item>
-            </el-menu-item-group>
-            <el-submenu index="2-4">
-              <template slot="title">选项4</template>
-              <el-menu-item index="2-4-1">选项4-1</el-menu-item>
-            </el-submenu>
           </el-submenu>
           <el-submenu index="3">
             <template slot="title"><i class="el-icon-setting"></i>
               <span slot="title">导航三</span>
             </template>
             <el-menu-item-group>
-              <template slot="title">分组一</template>
               <el-menu-item index="3-1">选项1</el-menu-item>
               <el-menu-item index="3-2">选项2</el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="分组2">
-              <el-menu-item index="3-3">选项3</el-menu-item>
             </el-menu-item-group>
             <el-submenu index="3-4">
               <template slot="title">选项4</template>
@@ -79,14 +61,49 @@
         </el-header>
 
         <el-main>
-          <el-table :data="tableData">
+          <div style="margin-bottom: 15px">
+            <el-breadcrumb separator-class="el-icon-arrow-right">
+              <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+              <el-breadcrumb-item>活动管理</el-breadcrumb-item>
+            </el-breadcrumb>
+          </div>
+          <div style="padding: 10px 0">
+            <el-input style="width: 200px" placeholder="请输入信息" suffix-icon="el-icon-search" ></el-input>
+            <el-input style="width: 200px" placeholder="请输入信息" suffix-icon="el-icon-message" class="ml-5"></el-input>
+            <el-input style="width: 200px" placeholder="请输入信息" suffix-icon="el-icon-position" class="ml-5" ></el-input>
+            <el-button class="ml-5" type="primary">搜素</el-button>
+          </div>
+          <div style="margin: 10px 0">
+            <el-button type="primary">新增<i class="el-icon-circle-plus-outline"></i></el-button>
+            <el-button type="danger">批量删除<i class="el-icon-remove-outline"></i></el-button>
+            <el-button type="primary">导入<i class="el-icon-bottom"></i></el-button>
+            <el-button type="primary">导出<i class="el-icon-top"></i></el-button>
+          </div>
+          <el-table :data="tableData" stripe>
             <el-table-column prop="date" label="日期" width="140">
             </el-table-column>
             <el-table-column prop="name" label="姓名" width="120">
             </el-table-column>
             <el-table-column prop="address" label="地址">
             </el-table-column>
+            <el-table-column label="操作">
+              <template slot-scope="scope">
+                <el-button type="success">编辑<i class="el-icon-edit"></i></el-button>
+                <el-button type="danger">删除<i class="el-icon-delete"></i></el-button>
+              </template>
+            </el-table-column>
           </el-table>
+          <div style="padding: 10px 0">
+            <el-pagination
+                @size-change="handleSizeChange"
+                @current-change="handleCurrentChange"
+                :current-page="currentPage4"
+                :page-sizes="[5, 10, 15, 20]"
+                :page-size="10"
+                layout="total, sizes, prev, pager, next, jumper"
+                :total="40">
+            </el-pagination>
+          </div>
         </el-main>
       </el-container>
     </el-container>
