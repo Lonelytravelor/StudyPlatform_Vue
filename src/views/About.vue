63cf0eb9a02e0b46c66be7c0b3dd410ffae72fbb
Pandora <1746384966@qq.com>
2022-02-16 00:08:03 +0800

    实现了登录页面，并实现了简单的登录功能（初步），并解决了无法获取字符串传参时的错误

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,65 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <el-card class="box-card">
+    <div>
+      <el-form label-width="80px">
+        <el-form-item label="活动名称">
+          <el-input v-model="userName" style="width: 140px"></el-input>
+        </el-form-item>
+        <el-form-item label="活动名称" >
+          <el-input v-model="password" style="width: 140px"></el-input>
+        </el-form-item>
+      </el-form>
+      <el-button type="primary" @click="login">登 录</el-button>
+    </div>
+  </el-card>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "About",
+  data(){
+    return{
+      userName: "",
+      password: ""
+    }
+  },
+  methods: {
+    login: function () {
+      var that = this;
+      axios({
+        url: "http://localhost:9090/login",
+        method: "post",
+        data: {
+          userName: this.userName,
+          password: this.password,
+        },
+        headers: {
+          "Content-type": "application/json;charset=utf-8"
+        },
+      }).then(function (response){
+        if ( response.data ){
+          alert("Success");
+        }else {
+          alert("Fail");
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.text {
+  font-size: 14px;
+}
+
+.item {
+  padding: 18px 0;
+}
+
+.box-card {
+  width: 480px;
+}
+</style>
---
+++ src/views/Index.vue
@@ -0,0 +1,76 @@
+<template>
+  <el-container>
+    <el-header >
+      <el-menu  class="el-menu-demo nav" mode="horizontal" router text-color="#000000" style="width: 60% ;margin-left: 30%">
+        <el-menu-item index="/">首页</el-menu-item>
+        <el-menu-item index="/">课程</el-menu-item>
+        <el-menu-item index="/">学校</el-menu-item>
+      </el-menu>
+      <span>王小虎</span><i class="el-icon-arrow-down" style="margin-left: 5px"></i>
+    </el-header>
+    <el-dialog title="收货地址" :visible.sync="dialogFormVisible" width="35%" >
+      <el-form :model="form" label-width="80px">
+        <el-form-item label="活动名称">
+          <el-input v-model="form.userEmail" style="width: 140px"></el-input>
+        </el-form-item>
+        <el-form-item label="活动名称" >
+          <el-input v-model="form.userPhone" style="width: 140px"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+      </div>
+    </el-dialog>
+    <el-button type="text" @click="dialogFormVisible = true">打开嵌套表单的 Dialog</el-button>
+  </el-container>
+
+</template>
+
+<script>
+export default {
+  name: "Index",
+  data() {
+    return {
+      dialogFormVisible : false,
+      form: {}
+    }
+  }
+}
+</script>
+
+<style scoped>
+.el-header, .el-footer {
+  background-color: #ffffff;
+  color: #333;
+  line-height: 60px;
+  box-sizing: border-box;
+}
+
+.el-aside {
+  background-color: #D3DCE6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-main {
+  background-color: #E9EEF3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
+}
+</style>
