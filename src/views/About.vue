b219f461649738112c0731f7e8e583c4f5a9d920
Pandora <1746384966@qq.com>
2022-02-16 19:21:29 +0800

    实现了检查输入的密码是否为空功能

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,32 +1,36 @@
 <template>
-  <el-card class="box-card">
-    <div v-show="isLogin">
-      <el-form label-width="80px">
-        <el-form-item label="用户名">
-          <el-input v-model="userName" style="width: 140px"></el-input>
-        </el-form-item>
-        <el-form-item label="密码" >
-          <el-input v-model="password" style="width: 140px"></el-input>
-        </el-form-item>
-      </el-form>
-      <el-button type="primary" @click="toRegister">没有账号，我要注册</el-button>
-      <el-button type="primary" @click="login">登 录</el-button>
-    </div>
-    <div v-show="isRegister">
-      <el-form label-width="80px" >
-        <el-form-item label="手机号">
-          <el-input v-model="userName" style="width: 140px"></el-input>
-        </el-form-item>
-        <el-form-item label="密码" >
-          <el-input v-model="password" style="width: 140px"></el-input>
-        </el-form-item>
-        <el-form-item label="确认密码" >
-          <el-input v-model="confirm_password" style="width: 140px"></el-input>
-        </el-form-item>
-      </el-form>
-      <el-button type="primary" @click="register">注 册</el-button>
-    </div>
-  </el-card>
+  <el-container>
+    <el-main>
+      <el-card class="box-card">
+        <div v-show="isLogin">
+          <el-form label-width="80px">
+            <el-form-item label="用户名">
+              <el-input v-model="userName" style="width: 140px"></el-input>
+            </el-form-item>
+            <el-form-item label="密码" >
+              <el-input v-model="password" style="width: 140px"></el-input>
+            </el-form-item>
+          </el-form>
+          <el-button type="primary" @click="toRegister">没有账号，我要注册</el-button>
+          <el-button type="primary" @click="login">登 录</el-button>
+        </div>
+        <div v-show="isRegister">
+          <el-form label-width="80px" >
+            <el-form-item label="手机号">
+              <el-input v-model="userName" style="width: 140px"></el-input>
+            </el-form-item>
+            <el-form-item label="密码" >
+              <el-input v-model="password" style="width: 140px"></el-input>
+            </el-form-item>
+            <el-form-item label="确认密码" >
+              <el-input v-model="confirm_password" style="width: 140px"></el-input>
+            </el-form-item>
+          </el-form>
+          <el-button type="primary" @click="register">注 册</el-button>
+        </div>
+      </el-card>
+    </el-main>
+  </el-container>
 </template>
 
 <script>
@@ -54,6 +58,17 @@ export default {
         res = false;
         this.$message({
           message: '两次密码不一致！',
+          type: 'warning'
+        });
+      }
+      return res;
+    },
+    checkPasswordNotNull: function () {
+      var res = true;
+      if ( this.password === "" ){
+        res = false;
+        this.$message({
+          message: '请输入密码！',
           type: 'warning'
         });
       }
@@ -93,7 +108,7 @@ export default {
     },
     register: function () {
       var that = this;
-      if ( this.checkPhone() && this.checkPassword() ){
+      if ( this.checkPhone() && this.checkPasswordNotNull() && this.checkPassword()){
         axios({
           url: "http://localhost:9090/register",
           method: "post",
