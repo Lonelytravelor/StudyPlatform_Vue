24552035b41a835027245e4e0cec763973e4ce64
Pandora <1746384966@qq.com>
2022-04-03 22:36:38 +0800

    实现了基础的目录加载

--- src/views/CourseWareIndex.vue
+++ src/views/CourseWareIndex.vue
@@ -4,18 +4,19 @@
       <div slot="header" class="clearfix">
         <span>课件</span>
       </div>
-      <el-collapse style="width: 98%; margin-left: 1%;">
-        <el-collapse-item v-for="(point, index) in points" :key="point">
+      <el-collapse accordion style="width: 98%; margin-left: 1%;">
+        <el-collapse-item v-for="(point1, index) in courseIndex" :key="point1.id">
           <template slot="title">
-            <span style="font-size: 18px" >第{{ index+1 }}章 {{ point }}</span>
+            <span style="font-size: 18px" >第{{ index+1 }}章 {{ point1.name }}</span>
           </template>
           <div style="margin-left: 20px">
-            <span style="font-size: 16px">第一节 创建者模式</span>
-            <div style="margin-left: 20px">
-              <div>原型模式</div>
-              <div>原型模式</div>
-              <div>原型模式</div>
-              <div>原型模式</div>
+            <div v-for="(point2, index) in point1.nextPoints" :key="point2.id">
+              <span style="font-size: 16px">第{{ index+1 }}节 {{ point2.name }}</span>
+              <div style="margin-left: 20px">
+                <div v-for="(point3, index) in point2.nextPoints" :key="point3.id">
+                  <el-link @click="ToCourseWareShow">{{ point3.name }}</el-link>
+                </div>
+              </div>
             </div>
           </div>
         </el-collapse-item>
@@ -26,6 +27,7 @@
 
 <script>
 import router from "@/router";
+import axios from "axios";
 
 export default {
   name: "CourseWareIndex",
@@ -34,7 +36,21 @@ export default {
       isShow : true,
       courseWareId: 1,
       points: ["建造者模式", "单例模式", "工厂模式", "原型模式"],
+      courseIndex: [],
     }
+  },
+  created() {
+    let that = this;
+    axios({
+      url: "http://localhost:9090/loadCoursePointIndex",
+      params: {
+        courseId: this.$store.state.courseId,
+        // courseId: 1,
+      }
+    }).then(function (response) {
+      console.log(response.data);
+      that.courseIndex = response.data.nextPoints;
+    })
   },
   methods: {
     ToCourseWareShow() {
